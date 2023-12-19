abf7bfa64e0bd20111afbed1a03dff103a5813a1
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-12-19 14:39:53 -0500

    queen is working

--- main.py
+++ main.py
@@ -307,8 +307,11 @@ def check_bishop(position, color):
 
 
 def check_queen(position, color):
-    pass
-
+    moves_list = check_bishop(position, color)
+    second_list = check_rook(position, color)
+    for i in range(len(second_list)):
+        moves_list.append(second_list[i])
+    return moves_list
 
 def check_king(position, color):
     pass
