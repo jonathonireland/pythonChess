ee0948aabc7336bca0db5999e3726703a9800410
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-12-19 12:57:40 -0500

    Knight functional moves

--- main.py
+++ main.py
@@ -258,7 +258,18 @@ def check_rook(position, color):
 
 
 def check_knight(position, color):
-    pass
+    moves_list = []
+    if color == 'white':
+        friends_list = white_locations
+    elif color == 'black':
+        friends_list = black_locations
+    # 8 squares to check for knights, they can go two squares in one direction and one in another
+    targets = [(1, 2), (1, -2), (2, 1), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+    for i in range(8):
+        target = (position[0] + targets[i][0], position[1] + targets[i][1])
+        if target not in friends_list and 0 <= target[0] <= 7 and 0 <= target[1] <= 7:
+            moves_list.append(target)
+    return moves_list
 
 
 def check_bishop(position, color):
