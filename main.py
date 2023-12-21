ab0a1e84545d171a491c33d44c965fc44422aaa8
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-12-20 21:42:07 -0500

    adding game over logic

--- main.py
+++ main.py
@@ -436,5 +436,28 @@ while run:
                     turn_step = 0
                     selection = 100
                     valid_moves = []
+        if event.type == pygame.KEYDOWN and game_over:
+            if event.key == pygame.K_RETURN:
+                game_over = False
+                winner = ''
+                white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+                white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                                (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+                black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+                black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+                                (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+                captured_pieces_white = []
+                captured_pieces_black = []
+                turn_step = 0
+                # 0 - whites turn no selction: 1- whites turn piece selected: 2 - black turn no selection
+                selection = 100
+                valid_moves = []
+                
+                black_options = check_options(black_pieces, black_locations, 'black')
+
+                white_options = check_options(white_pieces, white_locations, 'white')
+
     pygame.display.flip()
 pygame.quit()
