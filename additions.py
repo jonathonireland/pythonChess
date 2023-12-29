77a74933b604498bab5fdd55ca79c1cc6185a599
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-12-29 01:53:40 -0500

    tracking if pieces moved for castling

--- additions.py
+++ additions.py
@@ -417,6 +417,7 @@ while run:
                 if click_coords in valid_moves and selection != 100:
                     white_ep = check_ep(white_locations[selection], click_coords)
                     white_locations[selection] = click_coords
+                    white_moved[selection] = True
                     if click_coords in black_locations:
                         black_piece = black_locations.index(click_coords)
                         captured_pieces_white.append(black_pieces[black_piece])
@@ -424,11 +425,13 @@ while run:
                             winner = 'white'
                         black_pieces.pop(black_piece)
                         black_locations.pop(black_piece)
+                        black_moved.pop(black_piece)
                     if click_coords == black_ep:
                         black_piece = black_locations.index((black_ep[0], black_ep[1]-1))
                         captured_pieces_white.append(black_pieces[black_piece])
                         black_pieces.pop(black_piece)
                         black_locations.pop(black_piece)
+                        black_moved.pop(black_piece)
                     black_options = check_options(black_pieces, black_locations, 'black')
                     white_options = check_options(white_pieces, white_locations, 'white')
                     turn_step = 2
@@ -444,6 +447,7 @@ while run:
                 if click_coords in valid_moves and selection != 100:
                     black_ep = check_ep(black_locations[selection], click_coords)
                     black_locations[selection] = click_coords
+                    black_moved[selection] = True
                     if click_coords in white_locations:
                         white_piece = white_locations.index(click_coords)
                         captured_pieces_black.append(white_pieces[white_piece])
@@ -451,11 +455,13 @@ while run:
                             winner = 'black'
                         white_pieces.pop(white_piece)
                         white_locations.pop(white_piece)
+                        white_moved.pop(white_piece)
                     if click_coords == white_ep:
                         white_piece = white_locations.index((white_ep[0], white_ep[1]+1))
                         captured_pieces_black.append(white_pieces[white_piece])
                         white_pieces.pop(white_piece)
                         white_locations.pop(white_piece)
+                        white_moved.pop(white_piece)
                     black_options = check_options(black_pieces, black_locations, 'black')
                     white_options = check_options(white_pieces, white_locations, 'white')
                     turn_step = 0
@@ -469,10 +475,14 @@ while run:
                                 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
                 white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
                                 (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+                white_moved = [False, False, False, False, False, False, False, False, 
+                               False, False, False, False, False, False, False, False]
                 black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
                                 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
                 black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
                                 (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+                black_moved = [False, False, False, False, False, False, False, False, 
+                              False, False, False, False, False, False, False, False]
                 captured_pieces_white = []
                 captured_pieces_black = []
                 turn_step = 0
